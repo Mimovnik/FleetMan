9e8d2dac1b5ea773868faca31bb0d4c0442ed141
mimovnik <mimovnik@protonmail.com>
2025-04-22 15:02:17 +0200

    feat(app/register-passenger-ship): implement command validator
    Pass all the test.

--- FleetMan.Application/Registration/RegisterPassengerShip/RegisterPassengerShipCommandValidator.cs
+++ FleetMan.Application/Registration/RegisterPassengerShip/RegisterPassengerShipCommandValidator.cs
@@ -1,3 +1,4 @@
+using FleetMan.Domain.Errors;
 using FluentValidation;
 
 namespace FleetMan.Application.Registration.RegisterPassengerShip;
@@ -6,6 +7,14 @@ public class RegisterPassengerShipCommandValidator : AbstractValidator<RegisterP
 {
     public RegisterPassengerShipCommandValidator()
     {
-        // TODO: Add validation rules for the command properties
+        RuleFor(x => x.ImoNumber)
+            .Length(7).WithMessage(Errors.Ship.InvalidImoFormat.Description)
+            .Matches(@"^\d{7}$").WithMessage(Errors.Ship.InvalidImoFormat.Description);
+
+        RuleFor(x => x.Length)
+            .GreaterThan(0).WithMessage(Errors.Ship.InvalidLength.Description);
+
+        RuleFor(x => x.Width)
+            .GreaterThan(0).WithMessage(Errors.Ship.InvalidWidth.Description);
     }
 }
