fe1283d3464ca23e9857595799de822cffd20f81
mimovnik <mimovnik@protonmail.com>
2025-04-25 16:37:15 +0200

    test(api/register-passenger-ship): add automatic tests

---
+++ FleetMan.Api.Tests/Registration/RegisterPassengerShipTests.cs
@@ -0,0 +1,186 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using ErrorOr;
+using FleetMan.Contracts.Registration;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace FleetMan.Api.Tests.Registration;
+
+public class ShipsControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    
+    public class ProblemDetailsWithErrors : ProblemDetails
+    {
+        [JsonPropertyName("errors")]
+        public required Dictionary<string, string[]> Errors { get; set; }
+    }
+
+    [Fact]
+    public async Task Given_ValidInput_When_RegisteringNewShip_Then_ReturnsResult()
+    {
+        // Arrange
+        var request = new RegisterPassengerShipRequest(
+           ImoNumber: "1234567",
+           Name: "Black Pearl",
+           Length: 100,
+           Width: 20
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/ships/passenger", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var responseData = await response.Content.ReadFromJsonAsync<RegisterShipResponse>();
+
+        responseData.Should().NotBeNull();
+        responseData.ImoNumber.Should().Be(request.ImoNumber);
+        responseData.Name.Should().Be(request.Name);
+    }
+
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(-10, 0)]
+    [InlineData(0, -1023.4)]
+    [InlineData(-123.4, -432.1)]
+    public async Task Given_InvalidBothWidthAndLength_When_RegisteringNewShip_Then_BadRequestWithErrors(float invalidLength, float invalidWidth)
+    {
+        // Arrange
+        var request = new RegisterPassengerShipRequest(
+           ImoNumber: "0000000",
+           Name: "Titanic",
+           Length: invalidLength,
+           Width: invalidWidth
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/ships/passenger", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+
+        problemDetails.Errors.Should().ContainKey("Ship.NegativeWidth");
+        problemDetails.Errors["Ship.NegativeWidth"].Should().Contain("Ship width must be greater than 0.");
+
+        problemDetails.Errors.Should().ContainKey("Ship.NegativeLength");
+        problemDetails.Errors["Ship.NegativeLength"].Should().Contain("Ship length must be greater than 0.");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("123456")]
+    [InlineData("12345678")]
+    [InlineData("1234567890121234567890123456789012345678901234567890123456789012345678901234567890345678901234567890123456789012345678901234567890")]
+    public async Task Given_ImoWrongLength_When_RegisteringNewShip_Then_BadRequestWithErrors(string longImoNumber)
+    {
+        // Arrange
+        var request = new RegisterPassengerShipRequest(
+           ImoNumber: longImoNumber,
+           Name: "Titanic",
+           Length: 100,
+           Width: 50
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/ships/passenger", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+
+        problemDetails.Errors.Should().ContainKey("Ship.InvalidImoFormat");
+        problemDetails.Errors["Ship.InvalidImoFormat"].Should().Contain("The IMO number must be 7 characters long and contain only digits.");
+    }
+
+    [Theory]
+    [InlineData("-234567")]
+    [InlineData("1a34567")]
+    [InlineData("abcdefg")]
+    [InlineData("123456\u2605")]
+    public async Task Given_ImoWithLetters_When_RegisteringNewShip_Then_BadRequestWithErrors(string lettersImoNumber)
+    {
+        // Arrange
+        var request = new RegisterPassengerShipRequest(
+           ImoNumber: lettersImoNumber,
+           Name: "Titanic",
+           Length: 100,
+           Width: 50
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/ships/passenger", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+
+        problemDetails.Errors.Should().ContainKey("Ship.InvalidImoFormat");
+        problemDetails.Errors["Ship.InvalidImoFormat"].Should().Contain("The IMO number must be 7 characters long and contain only digits.");
+    }
+
+    [Theory]
+    [InlineData("9074722")] // checksum = 139
+    [InlineData("7601233")] // checksum = 101
+    [InlineData("1234564")] // checksum = 77
+    [InlineData("1000005")] // checksum = 7
+    [InlineData("0100005")] // checksum = 6
+    [InlineData("0010006")] // checksum = 5
+    [InlineData("0001008")] // checksum = 4
+    [InlineData("0000102")] // checksum = 3
+    [InlineData("0000011")] // checksum = 2
+    [InlineData("0000001")] // checksum = 0
+    public async Task Given_InvalidImoChecksum_When_RegisteringNewShip_Then_BadRequestWithErrors(string wrongChecksumImoNumber)
+    {
+        // Arrange
+        var request = new RegisterPassengerShipRequest(
+           ImoNumber: wrongChecksumImoNumber,
+           Name: "Titanic",
+           Length: 100,
+           Width: 20
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/ships/passenger", request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+        problemDetails.Errors.Should().ContainKey("Ship.InvalidImoChecksum");
+        problemDetails.Errors["Ship.InvalidImoChecksum"].Should().Contain("The provided IMO number has invalid checksum.");
+    }
+}
