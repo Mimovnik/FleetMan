ffc7061d9c96273b5aae2567c437478742d1a0a1
mimovnik <mimovnik@protonmail.com>
2025-04-26 18:41:22 +0200

    feat(api/register-tanker-ship): issue endpoint

--- FleetMan.Api/Registration/ShipsController.cs
+++ FleetMan.Api/Registration/ShipsController.cs
@@ -1,5 +1,6 @@
 using FleetMan.Application.Registration.Common;
 using FleetMan.Application.Registration.RegisterPassengerShip;
+using FleetMan.Application.Registration.RegisterTankerShip;
 using FleetMan.Contracts.Registration;
 using MapsterMapper;
 using MediatR;
@@ -13,10 +14,24 @@ public class ShipsController(IMapper mapper, ISender mediator) : ApiController
     private readonly ISender _mediator = mediator;
     private readonly IMapper _mapper = mapper;
 
+
     [HttpPost("passenger")]
     public async Task<IActionResult> RegisterPassengerShip(RegisterPassengerShipRequest request)
     {
         var command = _mapper.Map<RegisterPassengerShipCommand>(request);
+
+        var result = await _mediator.Send(command);
+
+        return result.Match(
+            contact => Ok(_mapper.Map<RegisterShipResult>(contact)),
+            errors => Problem(errors)
+        );
+    }
+
+    [HttpPost("tanker")]
+    public async Task<IActionResult> RegisterTankerShip(RegisterTankerShipRequest request)
+    {
+        var command = _mapper.Map<RegisterTankerShipCommand>(request);
 
         var result = await _mediator.Send(command);
 
