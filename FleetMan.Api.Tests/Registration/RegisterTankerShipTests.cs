2bea5543ab3b28eb4334a4f0e3afcd7e97ebcd47
mimovnik <mimovnik@protonmail.com>
2025-04-26 18:40:48 +0200

    test(api/register-tanker-ship): add tests

--- FleetMan.Api.Tests/Registration/RegisterTankerShipTests.cs
+++ FleetMan.Api.Tests/Registration/RegisterTankerShipTests.cs
@@ -8,12 +8,12 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace FleetMan.Api.Tests.Registration;
 
-public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+public class RegisterTankerShipTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client = factory.CreateClient();
 
-    private const string _baseUrl = "/ships/passenger";
-    
+    private const string _baseUrl = "/ships/tanker";
+
     public class ProblemDetailsWithErrors : ProblemDetails
     {
         [JsonPropertyName("errors")]
@@ -24,11 +24,17 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
     public async Task Given_ValidInput_When_RegisteringNewShip_Then_ReturnsResult()
     {
         // Arrange
-        var request = new RegisterPassengerShipRequest(
+        var request = new RegisterTankerShipRequest(
            ImoNumber: "1234567",
            Name: "Black Pearl",
            Length: 100,
-           Width: 20
+           Width: 20,
+           Tanks: [
+               new Tank("Diesel", 1024.123f),
+               new Tank("HeavyFuel", 10),
+               new Tank("Diesel", 10000),
+               new Tank("HeavyFuel", 124.5f),
+           ]
         );
 
         // Act
@@ -52,11 +58,17 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
     public async Task Given_InvalidBothWidthAndLength_When_RegisteringNewShip_Then_BadRequestWithErrors(float invalidLength, float invalidWidth)
     {
         // Arrange
-        var request = new RegisterPassengerShipRequest(
+        var request = new RegisterTankerShipRequest(
            ImoNumber: "0000000",
            Name: "Titanic",
            Length: invalidLength,
-           Width: invalidWidth
+           Width: invalidWidth,
+           Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", 10),
+                new Tank("Diesel", 10000),
+                new Tank("HeavyFuel", 124.5f),
+              ]
         );
 
         // Act
@@ -88,11 +100,17 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
     public async Task Given_ImoWrongLength_When_RegisteringNewShip_Then_BadRequestWithErrors(string longImoNumber)
     {
         // Arrange
-        var request = new RegisterPassengerShipRequest(
-           ImoNumber: longImoNumber,
-           Name: "Titanic",
-           Length: 100,
-           Width: 50
+        var request = new RegisterTankerShipRequest(
+            ImoNumber: longImoNumber,
+            Name: "Titanic",
+            Length: 100,
+            Width: 50,
+            Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", 10),
+                new Tank("Diesel", 10000),
+                new Tank("HeavyFuel", 124.5f),
+            ]
         );
 
         // Act
@@ -121,11 +139,17 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
     public async Task Given_ImoWithLetters_When_RegisteringNewShip_Then_BadRequestWithErrors(string lettersImoNumber)
     {
         // Arrange
-        var request = new RegisterPassengerShipRequest(
-           ImoNumber: lettersImoNumber,
-           Name: "Titanic",
-           Length: 100,
-           Width: 50
+        var request = new RegisterTankerShipRequest(
+            ImoNumber: lettersImoNumber,
+            Name: "Titanic",
+            Length: 100,
+            Width: 50,
+            Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", 10),
+                new Tank("Diesel", 10000),
+                new Tank("HeavyFuel", 124.5f),
+            ]
         );
 
         // Act
@@ -160,11 +184,17 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
     public async Task Given_InvalidImoChecksum_When_RegisteringNewShip_Then_BadRequestWithErrors(string wrongChecksumImoNumber)
     {
         // Arrange
-        var request = new RegisterPassengerShipRequest(
-           ImoNumber: wrongChecksumImoNumber,
-           Name: "Titanic",
-           Length: 100,
-           Width: 20
+        var request = new RegisterTankerShipRequest(
+            ImoNumber: wrongChecksumImoNumber,
+            Name: "Titanic",
+            Length: 100,
+            Width: 20,
+            Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", 10),
+                new Tank("Diesel", 10000),
+                new Tank("HeavyFuel", 124.5f),
+            ]
         );
 
         // Act
@@ -182,5 +212,79 @@ public class RegisterPassengerShipTests(WebApplicationFactory<Program> factory) 
         problemDetails.Errors.Should().NotBeNull();
         problemDetails.Errors.Should().ContainKey("Ship.InvalidImoChecksum");
         problemDetails.Errors["Ship.InvalidImoChecksum"].Should().Contain("The provided IMO number has invalid checksum.");
+    }
+
+    [Theory]
+    [InlineData("Gasoline")] 
+    [InlineData("fuelOil")] 
+    [InlineData("")] 
+    public async Task Given_InvalidTankFuelType_When_RegisteringNewShip_Then_BadRequestWithErrors(string wrongFuelType)
+    {
+        // Arrange
+        var request = new RegisterTankerShipRequest(
+            ImoNumber: "1234567",
+            Name: "Titanic",
+            Length: 100,
+            Width: 20,
+            Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", 10),
+                new Tank(wrongFuelType, 10000),
+                new Tank(wrongFuelType, 124.5f),
+            ]
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync(_baseUrl, request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+        problemDetails.Errors.Should().ContainKey("Tank.InvalidFuelType");
+        problemDetails.Errors["Tank.InvalidFuelType"].Should().Contain("Fuel type must be one of: 'Diesel', 'HeavyFuel'.");
+    }
+
+    [Theory]
+    [InlineData(0)] 
+    [InlineData(-10)] 
+    [InlineData(-123.45f)] 
+    public async Task Given_InvalidTankCapacity_When_RegisteringNewShip_Then_BadRequestWithErrors(float wrongCapacity)
+    {
+        // Arrange
+        var request = new RegisterTankerShipRequest(
+            ImoNumber: "1234567",
+            Name: "Titanic",
+            Length: 100,
+            Width: 20,
+            Tanks: [
+                new Tank("Diesel", 1024.123f),
+                new Tank("HeavyFuel", wrongCapacity),
+                new Tank("Diesel", wrongCapacity),
+                new Tank("HeavyFuel", 124.5f),
+            ]
+        );
+
+        // Act
+        var response = await _client.PostAsJsonAsync(_baseUrl, request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetailsWithErrors>();
+
+        problemDetails.Should().NotBeNull();
+        problemDetails.Title.Should().Be("One or more validation errors occurred.");
+        problemDetails.Status.Should().Be(400);
+
+        problemDetails.Errors.Should().NotBeNull();
+        problemDetails.Errors.Should().ContainKey("Tank.NegativeCapacity");
+        problemDetails.Errors["Tank.NegativeCapacity"].Should().Contain("Tank capacity must be greater than 0.");
     }
 }
