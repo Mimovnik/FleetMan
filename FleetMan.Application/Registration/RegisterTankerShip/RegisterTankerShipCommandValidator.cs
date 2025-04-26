1c2774b80a92eda64e6d0a7c0d41698efe2745d2
mimovnik <mimovnik@protonmail.com>
2025-04-26 15:32:29 +0200

    feat(app/register-tanker-ship): implement command validator

--- FleetMan.Application/Registration/RegisterTankerShip/RegisterTankerShipCommandValidator.cs
+++ FleetMan.Application/Registration/RegisterTankerShip/RegisterTankerShipCommandValidator.cs
@@ -1,4 +1,6 @@
 using FleetMan.Application.Registration.Common;
+using FleetMan.Domain.Errors;
+using FluentValidation;
 
 namespace FleetMan.Application.Registration.RegisterTankerShip;
 
@@ -7,6 +9,24 @@ public class RegisterTankerShipCommandValidator
 {
     public RegisterTankerShipCommandValidator()
     {
-        // TODO: Add validation rules specific to tanker ships
+        RuleFor(x => x.ImoNumber)
+            .Length(7).WithMessage(Errors.Ship.InvalidImoFormat.Description)
+            .Matches(@"^\d{7}$").WithMessage(Errors.Ship.InvalidImoFormat.Description);
+
+        RuleFor(x => x.Length)
+            .GreaterThan(0).WithMessage(Errors.Ship.InvalidLength.Description);
+
+        RuleFor(x => x.Width)
+            .GreaterThan(0).WithMessage(Errors.Ship.InvalidWidth.Description);
+
+        RuleFor(x => x.Tanks)
+            .NotEmpty().WithMessage(Errors.Ship.TankerShip.NoTanks.Description);
+
+        RuleForEach(x => x.Tanks)
+            .ChildRules(tank =>
+            {
+                tank.RuleFor(x => x.Capacity)
+                    .GreaterThan(0).WithMessage(Errors.Tank.InvalidCapacity.Description);
+            });
     }
 }
