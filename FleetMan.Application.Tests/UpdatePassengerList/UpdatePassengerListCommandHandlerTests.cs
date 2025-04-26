8331b5f6897009aea38211a331b225dc8e0c5fd6
mimovnik <mimovnik@protonmail.com>
2025-04-27 00:51:13 +0200

    test(app/update-passenger-list): add command handler tests

---
+++ FleetMan.Application.Tests/UpdatePassengerList/UpdatePassengerListCommandHandlerTests.cs
@@ -0,0 +1,89 @@
+using FleetMan.Application.Interfaces;
+using FleetMan.Application.UpdatePassengerList;
+using FleetMan.Domain.Entities;
+using FleetMan.Domain.Errors;
+using FluentAssertions;
+using Moq;
+
+namespace FleetMan.Application.Tests.UpdatePassengerList;
+
+public class UpdatePassengerListCommandHandlerTests
+{
+    private readonly Mock<IShipRepository> _repositoryMock;
+    private readonly UpdatePassengerListCommandHandler _handler;
+
+    public UpdatePassengerListCommandHandlerTests()
+    {
+        _repositoryMock = new Mock<IShipRepository>();
+        _handler = new UpdatePassengerListCommandHandler(_repositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShipIsOfPassengerType_ReturnsNoError()
+    {
+        _repositoryMock
+            .Setup(r => r.GetByImoNumberAsync(It.IsAny<ImoNumber>()))
+            .ReturnsAsync(PassengerShip.Create(
+                ImoNumber.Create("9074729").Value,
+                name: "Passenger Ship",
+                length: 300,
+                width: 50
+            ).Value);
+
+        var command = new UpdatePassengerListCommand("9074729", [
+            "John Doe",
+            "Jane Boe",
+            "Jack Noe",
+        ]);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.IsError.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Handle_ShipIsOfTankerType_ReturnsNotPassengerShipError()
+    {
+        _repositoryMock
+            .Setup(r => r.GetByImoNumberAsync(It.IsAny<ImoNumber>()))
+            .ReturnsAsync(TankerShip.Create(
+                ImoNumber.Create("9074729").Value,
+                name: "Tanker Ship",
+                length: 300,
+                width: 50,
+                tanks: [
+                    Tank.Create(Tank.TankFuelType.Diesel, 1000).Value
+                ]
+            ).Value);
+
+        var command = new UpdatePassengerListCommand("9074729", [
+            "John Doe",
+            "Jane Boe",
+            "Jack Noe",
+        ]);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.IsError.Should().BeTrue();
+        result.Errors.Should().ContainSingle(e => e == Errors.Ship.NotPassenger);
+    }
+
+    [Fact]
+    public async Task Handle_ShipNotFound_ReturnsShipNotFoundError()
+    {
+        _repositoryMock
+            .Setup(r => r.GetByImoNumberAsync(It.IsAny<ImoNumber>()))
+            .ReturnsAsync(Errors.Ship.NotFound);
+
+        var command = new UpdatePassengerListCommand("9074729", [
+            "John Doe",
+            "Jane Boe",
+            "Jack Noe",
+        ]);
+
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        result.IsError.Should().BeTrue();
+        result.Errors.Should().ContainSingle(e => e == Errors.Ship.NotFound);
+    }
+}
