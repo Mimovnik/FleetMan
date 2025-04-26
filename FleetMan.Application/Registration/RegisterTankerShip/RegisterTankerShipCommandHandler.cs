15dca3415d2000cc1f6730e61e51b880b8dc30b5
mimovnik <mimovnik@protonmail.com>
2025-04-26 18:04:00 +0200

    feat(app/register-tanker-ship): implement command handler

--- FleetMan.Application/Registration/RegisterTankerShip/RegisterTankerShipCommandHandler.cs
+++ FleetMan.Application/Registration/RegisterTankerShip/RegisterTankerShipCommandHandler.cs
@@ -1,6 +1,9 @@
 using ErrorOr;
+using FleetMan.Application.DTOs;
 using FleetMan.Application.Interfaces;
 using FleetMan.Application.Registration.Common;
+using FleetMan.Domain.Entities;
+using FleetMan.Domain.Errors;
 using MediatR;
 
 
@@ -11,12 +14,60 @@ public class RegisterTankerShipCommandHandler(IShipRepository repository)
 {
     private readonly IShipRepository _repository = repository;
 
+    private static ErrorOr<List<Tank>> CreateTanks(List<TankDto> tankDtos)
+    {
+        var tankList = new List<Tank>();
+
+        foreach (var tank in tankDtos)
+        {
+            bool fuelTypeIsCorrect = Enum.TryParse(tank.FuelType, out Tank.TankFuelType fuelTypeResult);
+
+            if (!fuelTypeIsCorrect)
+                return Errors.Tank.InvalidFuelType;
+
+            var tankResult = Tank.Create(fuelTypeResult, tank.Capacity);
+
+            if (tankResult.IsError)
+                return tankResult.Errors;
+
+            tankList.Add(tankResult.Value);
+        }
+
+        return tankList;
+    }
+
     public async Task<ErrorOr<RegisterShipResult>> Handle(
         RegisterTankerShipCommand request,
         CancellationToken cancellationToken)
     {
-        await Task.CompletedTask;
-        // TODO: Implement the command handler logic
-        return Error.Unexpected();
+        var imoNumberResult = ImoNumber.Create(request.ImoNumber);
+        if (imoNumberResult.IsError)
+            return imoNumberResult.Errors;
+        var imoNumber = imoNumberResult.Value;
+
+        var tanksResult = CreateTanks(request.Tanks);
+        if (tanksResult.IsError)
+            return tanksResult.Errors;
+        var tanks = tanksResult.Value;
+
+        var shipResult = TankerShip.Create(
+            imoNumber,
+            request.Name,
+            request.Length,
+            request.Width,
+            tanks
+        );
+
+        if (shipResult.IsError)
+            return shipResult.Errors;
+
+        var ship = shipResult.Value;
+
+        if (await _repository.ExistsAsync(imoNumber))
+            return Errors.Ship.DuplicateImo;
+
+        await _repository.AddAsync(ship);
+
+        return new RegisterShipResult(ship.ImoNumber.ToString(), ship.Name);
     }
 }
