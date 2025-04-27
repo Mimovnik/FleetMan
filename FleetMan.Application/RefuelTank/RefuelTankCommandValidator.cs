ba8b9691fa16affe42172f2f23e6e1cb716a1b6a
mimovnik <mimovnik@protonmail.com>
2025-04-27 21:46:21 +0200

    feat(app/refuel-tank): implement command validator

--- FleetMan.Application/RefuelTank/RefuelTankCommandValidator.cs
+++ FleetMan.Application/RefuelTank/RefuelTankCommandValidator.cs
@@ -1,13 +1,30 @@
+using FleetMan.Domain.Errors;
 using FluentValidation;
+using static FleetMan.Domain.Entities.Tank;
 
 namespace FleetMan.Application.RefuelTank;
 
 public class RefuelTankCommandValidator
 : AbstractValidator<RefuelTankCommand>
 {
+    private static bool BeValidFuelType(string value)
+    {
+        bool parsable = Enum.TryParse(value, ignoreCase: true, out TankFuelType parsed);
+
+        return parsable && parsed != TankFuelType.Empty;
+    }
+
     public RefuelTankCommandValidator()
     {
-        // TODO: Add validation rules for the command properties
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+                .WithErrorCode(Errors.Tank.InvalidRefuelAmount.Code)
+                .WithMessage(Errors.Tank.InvalidRefuelAmount.Description);
+
+        RuleFor(x => x.FuelType)
+            .Must(BeValidFuelType)
+                .WithErrorCode(Errors.Tank.InvalidFuelType.Code)
+                .WithMessage(Errors.Tank.InvalidFuelType.Description);
     }
 }
 
