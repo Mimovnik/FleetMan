b6813f837310f9df22acd6359d89969ea20013ed
mimovnik <mimovnik@protonmail.com>
2025-04-28 00:43:41 +0200

    feat(app/drain-tank): implement command handler

--- FleetMan.Application/DrainTank/DrainTankCommandHandler.cs
+++ FleetMan.Application/DrainTank/DrainTankCommandHandler.cs
@@ -1,5 +1,8 @@
 using ErrorOr;
 using FleetMan.Application.Interfaces;
+using FleetMan.Domain.Entities;
+using FleetMan.Domain.Entities.Ships;
+using FleetMan.Domain.Errors;
 using MediatR;
 
 
@@ -14,8 +17,27 @@ public class DrainTankCommandHandler(IShipRepository repository)
         DrainTankCommand request,
         CancellationToken cancellationToken)
     {
-        await Task.CompletedTask;
-        // TODO: Implement the command handler logic
+        var imoResult = ImoNumber.Create(request.ImoNumber);
+        if (imoResult.IsError)
+            return imoResult.Errors;
+        var imoNumber = imoResult.Value;
+
+        var shipResult = await _repository.GetByImoNumberAsync(imoNumber);
+        if (shipResult.IsError)
+            return shipResult.Errors;
+        var ship = shipResult.Value;
+
+        if (ship is not TankerShip tankerShip)
+            return Errors.Ship.NotTanker;
+        
+        var tankIndex = request.TankNumber - 1;
+        if (tankIndex < 0 || tankIndex >= tankerShip.Tanks.Count)
+            return Errors.Tank.NotFound;
+
+        var tank = tankerShip.Tanks[tankIndex];
+
+        tank.Drain();
+
         return Unit.Value;
     }
 }
